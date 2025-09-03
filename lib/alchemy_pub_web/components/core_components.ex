4189741ecb8928d58402f89a48133154d3ee0656
Julian Köpke <julian.koepke@gmail.com>
2025-09-03 07:33:09 +0200

    formatter update

--- lib/alchemy_pub_web/components/core_components.ex
+++ lib/alchemy_pub_web/components/core_components.ex
@@ -170,7 +170,7 @@ defmodule AlchemyPubWeb.CoreComponents do
 
     assigns
     |> assign(field: nil, id: assigns.id || field.id)
-    |> assign(:errors, Enum.map(errors, &translate_error(&1)))
+    |> assign(:errors, Enum.map(errors, &translate_error/1))
     |> assign_new(:name, fn -> if assigns.multiple, do: field.name <> "[]", else: field.name end)
     |> assign_new(:value, fn -> field.value end)
     |> input()
