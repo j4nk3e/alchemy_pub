455fbac75beb6dd66782755048e07c62952c565d
Julian Köpke <julian.koepke@gmail.com>
2025-11-15 15:17:13 +0100

    upgrade to Elixir 1.19

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AlchemyPub.MixProject do
     [
       app: :alchemy_pub,
       version: "1.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
